ae0783f7946672dbff02773e72cda0d252516bdb
kk0510 <kousuke_k0510@yahoo.co.jp>
2012-10-25 18:31:04 +0900

    Update ExtractInformationFromDB.java
    他のプログラムで使えるように修正

--- ExtractInformationFromDB.java
+++ ExtractInformationFromDB.java
@@ -11,39 +11,30 @@ import java.util.*;
  * @author kosuke
  */
 public class ExtractInformationFromDB{
-    public static void main(String args[]){			
-	
-        int count =0;
-        
-        HashSet<String> hits = new HashSet<String>();
-       
-        try {
-		Connection conn = null;
-		Statement st = null;
-		Statement std = null;
-		ResultSet rs = null;
-		ResultSet rsd = null;
-		Class.forName("com.mysql.jdbc.Driver");
-		conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/wikipediaontology20111107","root","root");
-		st = conn.createStatement();
-		std = conn.createStatement();
-		rs = st.executeQuery("select * from instance_all");
+	public static HashMap<String, String> ExtractRelationsFromDB(){
+		HashMap<String, String> hits = new HashMap<String, String>();
+		 try {
+			Connection conn = null;
+			Statement st = null;
+			ResultSet rs = null;
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/wikipediaontology20111107","root","root");
+			st = conn.createStatement();
+			rs = st.executeQuery("select * from instance_all");
 
-		while(rs.next()){
-			hits.add(rs.getString("instance"));
+			while(rs.next()){
+				hits.put(rs.getString("instance"),rs.getString("class"));
+			}
+			System.out.println(hits.size());
+
+			rs.close();
+			st.close();
+			conn.close();
+		} catch (ClassNotFoundException e) {
+		System.out.println("ドライバを読み込めませんでした "+ e);
+		} catch (SQLException e) { 
+		System.out.println("データベース接続エラー　"+ e);
 		}
-		System.out.println(hits.size());
-		
-		rs.close();
-		st.close();
-		conn.close();
-        } catch (ClassNotFoundException e) {
-        System.out.println("ドライバを読み込めませんでした "+ e);
-        } catch (SQLException e) { 
-        System.out.println("データベース接続エラー　"+ e);
-        }     
-       
-                        
-
-}
+		return hits;
+	}
 }
